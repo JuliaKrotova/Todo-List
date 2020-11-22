a63c5e6535449c7ade94d8234c0db90fa9ac2ee6
Julia Krotova <julia9867@mail.ru>
2020-11-22 16:00:58 +0700

    добавляет список и модальные окна

---
+++ src/pages/index/index.ts
@@ -0,0 +1,26 @@
+import { getComponent } from '../../app/js/component';
+import Header from '../../components/header/header';
+
+const Index = {
+    namespace: 'index',
+    beforeEnter() {
+        this.header = new Header(getComponent('header'));
+    },
+    enter() {
+        // enter body
+    },
+    afterEnter() {
+        // afterEnter body
+    },
+    beforeLeave() {
+        // beforeLeave body
+    },
+    leave() {
+        // leave body
+    },
+    afterLeave() {
+        // afterLeave body
+    }
+}
+
+export default Index
